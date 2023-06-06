5ecfe8dd6d4503542dff60e931e8f5e4cf886478
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-06 12:33:27 +0100

    imported dependancies

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 from os import read 
 from flask import Flask, request, render_template
+
+import pandas as pd 
+import numpy as np 
+import neattext.functions as nfx 
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity, linear_kernel
+from dashboard import get_value_counts, get_level_count
+from dashboard import get_subjects_per_level, yearwise_profit
