14f557203b042798c0c5353a37a04643c51a811c
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-06-09 09:12:42 +0100

    get subjects function

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,15 @@ def get_value_counts(df):
 # get level count
 def get_level_count(df):
 
-    return dict(list(df.groupby(["level"])["num_subscribers"].count.items())[1:])
+    return dict(list(df.groupby(["level"])["num_subscribers"].count().items())[1:])
 
 
-#
+# get subjects per level
+def get_subjects_per_level(df):
+
+    ans = list(dict(df.groupby(['subject'])['level'].value_counts()).keys())
+    all_labels = [ans[i][0]+"_"+ans[i][1] for i in range(len(ans))]
+    all_values =list(dict(df.groupby(["subject"])["level"].value_counts()).values())
+    complete_dict = dict(zip(all_labels, all_values))
+
+    return complete_dict
